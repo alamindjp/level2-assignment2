543b0010cb346f4a86edc2d580f122a3ef7d0077
alamindjp <alaminhossaindjp@gmail.com>
2024-05-31 15:25:13 +0600

    try to solve zod error

--- dist/modules/products/product.interface.js
+++ dist/modules/products/product.interface.js
@@ -1,2 +1,3 @@
 "use strict";
+// export type TTags = string;
 Object.defineProperty(exports, "__esModule", { value: true });
--- src/modules/products/product.validation.ts
+++ src/modules/products/product.validation.ts
@@ -2,51 +2,51 @@ import { z } from 'zod';
 
 const zodValidationProduct = z.object({
   name: z.string({
-    required_error: 'Name is required',
-    invalid_type_error: 'Name must be a string',
+    required_error: 'Email is required',
+    invalid_type_error: 'Email must be a string',
   }),
-  description: z.string({
-    required_error: 'Description is required',
-    invalid_type_error: 'Description must be a string',
-  }),
-  price: z
-    .number({
-      required_error: 'Price is required',
-      invalid_type_error: 'Price must be a number',
-    })
-    .positive({ message: 'Price must be a positive number' }),
-  category: z.string({
-    required_error: 'Category is required',
-    invalid_type_error: 'Category must be a string',
-  }),
-  tags: z.string().array().nonempty({
-    message: "Tags Can't be empty!",
-  }),
-  variants: z
-    .object({
-      type: z.string({
-        required_error: 'Type is required',
-        invalid_type_error: 'Type must be a string',
-      }),
-      value: z.string({
-        required_error: 'Value is required',
-        invalid_type_error: 'Value must be a string',
-      }),
-    })
-    .array()
-    .nonempty({
-      message: "Variants Can't be empty!",
-    }),
-  inventory: z.object({
-    quantity: z.number({
-      required_error: 'Quantity is required',
-      invalid_type_error: 'Quantity must be a number',
-    }),
-    inStock: z.boolean({
-      required_error: 'inStock is required',
-      invalid_type_error: 'inStock must be a boolean',
-    }),
-  }),
+  // description: z.string({
+  //   required_error: 'Description is required',
+  //   invalid_type_error: 'Description must be a string',
+  // }),
+  // price: z
+  //   .number({
+  //     required_error: 'Price is required',
+  //     invalid_type_error: 'Price must be a number',
+  //   })
+  //   .positive({ message: 'Price must be a positive number' }),
+  // category: z.string({
+  //   required_error: 'Category is required',
+  //   invalid_type_error: 'Category must be a string',
+  // }),
+  // tags: z.string().array().nonempty({
+  //   message: "Tags Can't be empty!",
+  // }),
+  // variants: z
+  //   .object({
+  //     type: z.string({
+  //       required_error: 'Type is required',
+  //       invalid_type_error: 'Type must be a string',
+  //     }),
+  //     value: z.string({
+  //       required_error: 'Value is required',
+  //       invalid_type_error: 'Value must be a string',
+  //     }),
+  //   })
+  //   .array()
+  //   .nonempty({
+  //     message: "Variants Can't be empty!",
+  //   }),
+  // inventory: z.object({
+  //   quantity: z.number({
+  //     required_error: 'Quantity is required',
+  //     invalid_type_error: 'Quantity must be a number',
+  //   }),
+  //   inStock: z.boolean({
+  //     required_error: 'inStock is required',
+  //     invalid_type_error: 'inStock must be a boolean',
+  //   }),
+  // }),
 });
 
 export default zodValidationProduct;
