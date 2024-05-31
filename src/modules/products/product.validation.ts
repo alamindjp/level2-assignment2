fecda16c805fc4f7368b5238bac0812ddd0f0218
alamindjp <alaminhossaindjp@gmail.com>
2024-05-31 12:35:12 +0600

    try to solve zod error

--- src/modules/products/product.validation.ts
+++ src/modules/products/product.validation.ts
@@ -1,80 +1,57 @@
 import { z } from 'zod';
 
+// const tag = z.string({
+//   required_error: 'Tags is required',
+//   invalid_type_error: 'Tags must be a array of string',
+// });
+
 const zodValidationProduct = z.object({
-  name: z
-    .string({
-      required_error: 'Name is required',
-      invalid_type_error: 'Name must be a string',
-    })
-    .min(5, { message: 'Name must be 5 character' }),
-  description: z
-    .string({
-      required_error: 'Description is required',
-      invalid_type_error: 'Description must be a string',
-    })
-    .min(50, { message: 'description must be 50 character' }),
+  name: z.string({
+    required_error: 'Name is required',
+    invalid_type_error: 'Name must be a string',
+  }),
+  description: z.string({
+    required_error: 'Description is required',
+    invalid_type_error: 'Description must be a string',
+  }),
   price: z
     .number({
       required_error: 'Price is required',
       invalid_type_error: 'Price must be a number',
     })
-    .gt(0, { message: 'Price must be more than 0' }),
-  category: z
-    .string({
-      required_error: 'Category is required',
-      invalid_type_error: 'Category must be a string',
-    })
-    .min(4, { message: 'Category must be 4 character' }),
-  tags: z
-    .array(
-      z
-        .string({
-          required_error: 'Tags is required',
-          invalid_type_error: 'Tags must be a array of string',
-        })
-        .min(3, { message: 'Tag must be 3 character' }),
-    )
-    .min(3, { message: 'Minimum tags length 3' }),
+    .positive({ message: 'Price must be a positive number' }),
+  category: z.string({
+    required_error: 'Category is required',
+    invalid_type_error: 'Category must be a string',
+  }),
+  tags: z.string().array().nonempty({
+    message: "Tags Can't be empty!",
+  }),
   variants: z
-    .array(
-      z.object(
-        {
-          type: z.string({
-            required_error: 'Variants type is required',
-            invalid_type_error: 'Variants type must be a string',
-          }),
-          value: z.string({
-            required_error: 'Variants value is required',
-            invalid_type_error: 'Variants value must be a string',
-          }),
-        },
-        {
-          required_error: 'Variants is required',
-          invalid_type_error: 'Variants must be a array of objects',
-        },
-      ),
-      {
-        required_error: 'Variants is required',
-        invalid_type_error: 'Variants must be a array',
-      },
-    )
-    .min(2, { message: 'Minimum Variants length 2' }),
-  inventory: z.object(
-    {
-      quantity: z.number({
-        required_error: 'Quantity is required',
-        invalid_type_error: 'Quantity must be a number',
+    .object({
+      type: z.string({
+        required_error: 'Type is required',
+        invalid_type_error: 'Type must be a string',
       }),
-      inStock: z.boolean({
-        required_error: 'inStock is required',
-        invalid_type_error: 'inStock must be a boolean',
+      value: z.string({
+        required_error: 'Value is required',
+        invalid_type_error: 'Value must be a string',
       }),
-    },
-    {
-      required_error: 'Inventory is required',
-      invalid_type_error: 'Inventory must be a array',
-    },
-  ),
+    })
+    .array()
+    .nonempty({
+      message: "Variants Can't be empty!",
+    }),
+  inventory: z.object({
+    quantity: z.number({
+      required_error: 'Quantity is required',
+      invalid_type_error: 'Quantity must be a number',
+    }),
+    inStock: z.boolean({
+      required_error: 'inStock is required',
+      invalid_type_error: 'inStock must be a boolean',
+    }),
+  }),
 });
 
 export default zodValidationProduct;
